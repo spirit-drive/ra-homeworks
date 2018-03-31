b9a46893b52cf7433bc5e63d309bec600d19795b
Igor Zvyagin <spirit-drive@yandex.ru>
2018-03-31 23:57:34 +0300

    Вторая задача

--- state/layouts/js/App.js
+++ state/layouts/js/App.js
@@ -4,15 +4,33 @@ const VIEW_LIST = "view_list";
 const VIEW_MODULE = "view_module";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      icon: VIEW_LIST,
+      cardView: true
+    }
+  }
+
+  onSwitch = () => {
+    let cardView = this.state.icon === VIEW_MODULE;
+    let icon = cardView ? VIEW_LIST : VIEW_MODULE;
+
+    this.setState({
+        icon,
+        cardView,
+    });
+  };
+
   render() {
     return (
       <div>
         <div className="toolbar">
           <IconSwitch
-            icon={VIEW_MODULE}
-            onSwitch={() => console.log("сменился тип вывода")} />
+            icon={this.state.icon}
+            onSwitch={this.onSwitch} />
         </div>
-        {this.renderLayout(true)}
+        {this.renderLayout(this.state.cardView)}
       </div>
     );
   }
@@ -22,26 +40,27 @@ class App extends React.Component {
       return (
         <CardsView
           layout={this.props.layout}
-          cards={this.getShopItems(this.props.products, cardView)} />
+          cards={this.getShopItems(this.props.products, cardView)}
+        />
       );
     }
-    return (<ListView items={this.getShopItems(this.props.products, cardView)} />);
+    return <ListView items={this.getShopItems(this.props.products, cardView)} />;
   }
 
   getShopItems(products, cardView) {
+
     return products.map(product => {
-      let cardProps = {
-        title: product.name,
-        caption: product.color,
-        img: product.img,
-        price: `$${product.price}`
-      };
-      if (cardView) {
-        return (
-          <ShopCard {...cardProps}/>
-        );
-      }
-      return (<ShopItem {...cardProps}/>)
+
+      const Shop = cardView ? ShopCard : ShopItem;
+
+      return (
+        <Shop
+          title={product.name}
+          caption={product.color}
+          img={product.img}
+          price={`$${product.price}`}
+        />
+      )
     });
   }
 }
